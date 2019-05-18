32c11d084e52d39c8a3f228bb7d11c5b68cbc0c3
Deki <dekiakbar@linuxmail.org>
2019-05-18 22:55:11 +0700

    install sass,react-router, delete unused code

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+import {BrowserHistory} from 'react-router-dom';
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>
+          hello
+      </div>
+    )
+  }
+}
